ed0352e48e5f94db72c31eaa345ba80c1eca63a6
Belisnalva Costa <bella-costa@hotmail.com.br>
2023-11-16 18:54:04 -0300

    Descopactando Zip devlist

---
+++ devlist/src/test/java/com/bella/devSpuerior/devlist/DevlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bella.devSpuerior.devlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
