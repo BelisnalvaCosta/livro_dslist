34e86e34c83eaf489a22d47976bd916e5e08fb9e
Belisnalva Costa <bella-costa@hotmail.com.br>
2023-11-16 20:27:15 -0300

    Created files properties

---
+++ devlist/src/main/java/com/bella/devSpuerior/devlist/dto/LivroDTO.java
@@ -0,0 +1,13 @@
+package com.bella.devSpuerior.devlist.dto;
+
+public class LivroDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
