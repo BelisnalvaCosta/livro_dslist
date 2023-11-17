639268dbe6ed2ec703ffe70dd2e7d8dd760a66fd
Belisnalva Costa <bella-costa@hotmail.com.br>
2023-11-16 21:29:16 -0300

    Commit file Livro

--- devlist/src/main/java/com/bella/devSpuerior/devlist/entities/Livro.java
+++ devlist/src/main/java/com/bella/devSpuerior/devlist/entities/Livro.java
@@ -1,22 +1,45 @@
-package com.bella.devSpuerior.devlist.dto;
+package com.bella.devSpuerior.devlist.entities;
 
-import com.bella.devSpuerior.devlist.entities.Livro;
-import org.springframework.beans.BeanUtils;
+import jakarta.persistence.*;
 
-public class LivroDTO {
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "livro_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public LivroDTO(Livro entity) {
-        BeanUtils.copyProperties(entity, this);
+    public Livro(){
 
+    }
+
+    public Livro(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -89,5 +112,18 @@ public class LivroDTO {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Livro)) return false;
+        Livro livro = (Livro) o;
+        return id.equals(livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
